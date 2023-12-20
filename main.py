42365815f85ecc42fbbed4e2d6eb845be016cb1b
Ajay <ajaysajay@gmail.com>
2023-12-20 15:04:04 +0530

    Logging and DB Connection

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sys
+import inspect
+import logging
+
 from PySide6.QtWidgets import QApplication
 from mainwindow import MainWindow
-
+from db import CapexDB
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    logging.debug('%s.%s', __name__, inspect.currentframe().f_code.co_name)
+
+    capex_db = CapexDB()
+
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
-
-
-
-
-
-
