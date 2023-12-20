c826af68a143bb7eda0bd7a58b1b654ce591906a
Ajay <ajaysajay@gmail.com>
2023-12-20 15:03:45 +0530

    New Location function

--- mainwindow.py
+++ mainwindow.py
@@ -1,22 +1,16 @@
 from PySide6.QtWidgets import QMainWindow, QMdiSubWindow, QWidget
 from ui_mainwindow import Ui_MainWindow
-from location.ui_location import Ui_Form
+from location.widget_location import WidgetLocation
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.widget_location = None
         self.setupUi(self)
         self.actionLocation.triggered.connect(self.new_location)
 
-
     def new_location(self):
-        self.local = Ui_Form()
-        self.local_widget = QWidget()
-        self.local.setupUi(self.local_widget)
-        self.mdiArea.addSubWindow(self.local_widget)
-        self.local_widget.show()
-
-
-
-
+        self.widget_location = WidgetLocation()
+        self.mdiArea.addSubWindow(self.widget_location)
+        self.widget_location.show()
